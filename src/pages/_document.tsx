441cfb1d753dfc0af97652c46a6a683ff3dddb14
Pablo <pablolucio_@hotmail.com>
2022-06-19 12:10:32 -0300

    Improved performance#8

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,6 +14,8 @@ export default class MyDocument extends Document {
                     <meta name="keywords" content="pablosilvadev " />
                     <meta name="author" content="pablosilvadev" />
                     <meta name="author" content="pablosilvadev" />
+                    <meta name="theme-color" content="#000000" />
+                    <link rel="preload" as="font"></link>
                     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600;700;800&family=Poppins:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet"></link>
                 </Head>
                 <body>
